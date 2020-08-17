670a40a54eaefde92f41c607e4c13af64aa88586
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-17 22:56:40 +0200

    ok

---
+++ main.js
@@ -0,0 +1,33 @@
+const geoTz = require("geo-tz");
+
+//console.log("Utilisation de geo-tz");
+
+const raw = require("./citys.json");
+let data = JSON.parse(JSON.stringify(raw));
+let j = "";
+let arr = [];
+// Structure
+// "country":
+// "name": 
+// "lat":
+// "lng":
+data.forEach(d => {
+	let tz = geoTz(d.lat, d.lng);
+	let city = {
+		country: d.country,
+		name: d.name,
+		lat: parseFloat(d.lat),
+		lng: parseFloat(d.lng),
+		time_zone_name: tz
+	}
+	arr.push(city);
+	if (tz.length > 1) {
+		console.log(d.name + " -> " + tz);
+	}
+	/*if (tz.length == 0) {
+		console.log(d.name + " NO TIMEZONE");
+	}*/
+	///j = d.name + " - " + d.country + " - " + tz + " \n" + j;
+});
+let data_final = JSON.stringify(arr);
+//console.log(data_final);
