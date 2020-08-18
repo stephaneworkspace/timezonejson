fc05ae0cfd2ce6d6671595a1fcc7f3bfc8975909
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-18 23:31:48 +0200

    bricolage node.js json

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-const geoTz = require("geo-tz");
 
-//console.log("Utilisation de geo-tz");
+let arr = [];
+
+/*const geoTz = require("geo-tz");
+
+	//console.log("Utilisation de geo-tz");
 
 const raw = require("./citys.json");
 let data = JSON.parse(JSON.stringify(raw));
 let j = "";
 let arr = [];
-// Structure
-// "country":
-// "name": 
-// "lat":
-// "lng":
+	// Structure
+	// "country":
+	// "name": 
+	// "lat":
+	// "lng":
 data.forEach(d => {
 	let tz = geoTz(d.lat, d.lng);
 	let city = {
@@ -24,10 +27,22 @@ data.forEach(d => {
 	if (tz.length > 1) {
 		console.log(d.name + " -> " + tz);
 	}
-	/*if (tz.length == 0) {
-		console.log(d.name + " NO TIMEZONE");
-	}*/
+	//if (tz.length == 0) {
+	//	console.log(d.name + " NO TIMEZONE");
 	///j = d.name + " - " + d.country + " - " + tz + " \n" + j;
 });
+*/
+//value / offset / arr(utc)
+const raw = require("./timezones.json");
+let data = JSON.parse(JSON.stringify(raw));
+data.forEach(d => {
+	let utc_tz = {
+		text: d.value,
+		offset: d.offset,
+		utc: d.utch
+	}
+	arr.push(utc_tz);
+	console.log(d);
+});
 let data_final = JSON.stringify(arr);
-//console.log(data_final);
+console.log(data_final);
